2f2b36d40c414c8e69e7668e69ad4b4ffdc59b92
jasonhargrove <jason@objas.com>
2013-11-03 12:37:54 -0500

    feature(index) : add time to read calculation to index theme; and styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,91 @@
+
+
 /**
- * Main JS file for Casper behaviours
+ * Main JS file for unnamed index behaviours
  */
 
-/*globals jQuery, document */
+/* jQuery document */
+
 (function ($) {
+
     "use strict";
+
 
     $(document).ready(function(){
 
-        // On the home page, move the blog icon inside the header 
-        // for better relative/absolute positioning.
+        /**
+        * use facade function to produce the 'minutes to read'
+        * content on each excerpt **/
 
-        //$("#blog-logo").prependTo("#site-head-content");
+        showHowManyMinutes();
+
+
+        /** facade **/
+
+        // Get the content from the theme,
+        // // calculate time to read,
+        // // // add time content to the theme
+
+        function showHowManyMinutes () {
+
+          // Get excerpt containers
+
+          var excerpts = document.getElementsByClassName("post-excerpt-container");
+
+          for (var i = 0; i < excerpts.length; i++) {
+
+            // Get hidden content
+
+            // // Makes use of the {{content}} exposed to handlebars
+            // // // dumped in a div and hudden because we don't want to hack Ghost
+
+            var hiddenContent = excerpts[i].getElementsByClassName("hidden")[0]
+
+            // produce time to read string
+
+              , timeString = produceTimeString(stripHTML(hiddenContent.innerHTML));
+
+            // remove hidden content
+
+            excerpts[i].removeChild(hiddenContent);
+
+            // Add time to read to the action div content
+
+            var action = excerpts[i].getElementsByClassName("action")[0];
+            action.innerHTML += ' · ' + timeString;
+
+          };
+
+        }
+
+        /** services **/
+
+        function stripHTML (html) {
+          var tmp = document.createElement("DIV");
+          tmp.innerHTML = html;
+          return tmp.textContent || tmp.innerText || "";
+        }
+
+        function countWords (words) {
+          var s = words;
+          s = s.replace(/(^\s*)|(\s*$)/gi,"");
+          s = s.replace(/[ ]{2,}/gi," ");
+          s = s.replace(/\n /,"\n");
+          return s.split(" ").length;
+        }
+
+        function countTime (count) {
+          // average adult reads 250 words per minute
+          // src: http://en.wikipedia.org/wiki/Words_per_minute
+          return count / 250
+        }
+
+        function produceTimeString (words) {
+          return Math.ceil(countTime(countWords(words))) + " min to read"
+        }
+
 
     });
 
 }(jQuery));
+
